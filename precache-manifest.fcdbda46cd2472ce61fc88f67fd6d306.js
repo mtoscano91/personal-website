723165293757eb2e30bff2139881329d05c3b637
Manu <manueltoscano91@hotmail.com>
2020-08-17 17:31:20 +0200

    Updates

---
+++ precache-manifest.fcdbda46cd2472ce61fc88f67fd6d306.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60cafa35f0fde3e210cb16edfdd3c496",
+    "url": "/personal-website/index.html"
+  },
+  {
+    "revision": "2db174e332004858afca",
+    "url": "/personal-website/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "e08503627ec5640b8da6",
+    "url": "/personal-website/static/css/main.5846eee0.chunk.css"
+  },
+  {
+    "revision": "2db174e332004858afca",
+    "url": "/personal-website/static/js/2.bdc6885d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/personal-website/static/js/2.bdc6885d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e08503627ec5640b8da6",
+    "url": "/personal-website/static/js/main.9bba83be.chunk.js"
+  },
+  {
+    "revision": "d9385b05e5e57d9628fe",
+    "url": "/personal-website/static/js/runtime-main.3731dab7.js"
+  }
+]);
