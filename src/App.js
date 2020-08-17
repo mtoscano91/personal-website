4e7501c79e437b6912c5901675683493e01f557a
Manu <manueltoscano91@hotmail.com>
2020-08-17 17:14:01 +0200

    added landing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import React from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-// import { Route, Switch } from "react-router-dom";
 import HeaderComponent from "./components/header/HeaderComponent";
 import ProfileComponent from "./components/profile/ProfileComponent";
 import ProjectsComponent from "./components/projects/ProjectsComponent";
 import ExperienceComponent from "./components/experience/ExperienceComponent";
 import SkillsComponent from "./components/skills/SkillsComponent";
+import LandingComponent from "./components/landing/LandingComponent";
 
 function App() {
   return (
     <div className="App">
+      <LandingComponent />
       <HeaderComponent />
       <ProfileComponent />
       <ExperienceComponent />
       <SkillsComponent />
       <ProjectsComponent />
-      {/* <Route exact path="/" render={() => <ProfileComponent />} />
-      <Route exact path="/projects" component={ProjectsComponent} />
-      <Route exact path="/cv" render={() => <CVComponent />} /> */}
     </div>
   );
 }
---
+++ src/components/landing/LandingComponent.js
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+
+export default class LandingComponent extends Component {
+  render() {
+    return (
+      <div
+        id="top"
+        className="landing"
+        data-src="./Landing img.jpeg"
+        data-position="center right"
+        style={{ height: 618 + "px" }}
+      >
+        <div className="container">
+          <h1>Manuel Toscano</h1>
+          <p className="lead">Personal Website</p>
+        </div>
+
+        <div className="overlay"></div>
+
+        <a href="#profile" className="scroll-down">
+          <span className="glyphicon glyphicon-chevron-down"></span>
+        </a>
+      </div>
+    );
+  }
+}
