571fa4b11487fcb4cdb73410e54571e38130fc48
Manu <manueltoscano91@hotmail.com>
2020-08-17 18:19:51 +0200

    fixed minor bug

--- src/components/landing/LandingComponent.js
+++ src/components/landing/LandingComponent.js
@@ -3,13 +3,7 @@ import React, { Component } from "react";
 export default class LandingComponent extends Component {
   render() {
     return (
-      <div
-        id="top"
-        className="landing"
-        data-src="./Landing img.jpeg"
-        data-position="center right"
-        style={{ height: 618 + "px" }}
-      >
+      <div id="top" className="landing" style={{ height: 618 + "px" }}>
         <div className="container">
           <h1>Manuel Toscano</h1>
           <p className="lead">Personal Website</p>
