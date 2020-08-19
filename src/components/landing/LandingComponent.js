4e1ecb87380edb08328c13b70d2c8f5d3a53cf42
Manu <manueltoscano91@hotmail.com>
2020-08-19 17:55:21 +0200

    img fix 4 grayscale

--- src/components/landing/LandingComponent.js
+++ src/components/landing/LandingComponent.js
@@ -1,17 +1,9 @@
 import React, { Component } from "react";
-import landingImg from "../../Landing2.jpeg";
 
 export default class LandingComponent extends Component {
   render() {
     return (
-      <div
-        id="top"
-        className="landing"
-        style={{
-          height: 618 + "px",
-          backgroundImage: landingImg,
-        }}
-      >
+      <div id="top" className="landing" style={{ height: 618 + "px" }}>
         <div className="container">
           <h1>Manuel Toscano</h1>
           <p className="lead">Personal Website</p>
